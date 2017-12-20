4446023d1f2a56f87eb463186d8ccc8a378de43c
Laurent Courty <lrntct@gmail.com>
2017-12-20 11:25:50 -0600

    update anaconda-upload

--- ci-tools/anaconda-upload.py
+++ ci-tools/anaconda-upload.py
@@ -5,7 +5,8 @@ import traceback
 
 token = os.environ['ANACONDA_TOKEN']
 cmd = ['anaconda', '-t', token, 'upload', '--force']
-packages = glob.glob('**\win-*\swashes-*.tar.bz2', recursive=True)
+packages = glob.glob('C:\conda\conda-bld\win-*\swashes-*.tar.bz2', recursive=True)
+
 cmd.extend(packages)
 try:
     subprocess.check_call(cmd)
